f25523b34b5aa77727cb505d72cf3277e06302de
alexjames12345 <210540@hkis.edu.hk>
2016-05-20 09:27:33 +0800

    bg image

--- gamedev.js
+++ gamedev.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-
+$("body").css("background-image", "http://mimsmadness.com/wordpress/wp-content/uploads/2013/01/bigstock-Baseball-on-fire-15689423.jpg")
 //Your jquery goes here
 
 });
